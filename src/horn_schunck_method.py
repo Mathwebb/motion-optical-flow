86b92794d7ae596ae874273cf68a52b696d0d132
Matheus Luís Webber <matheus.webber@hotmail.com>
2023-02-27 21:51:53 -0300

    Refactoring the algorithm to get a better precision the optical flow calculation and allowing the possibility to use the color informations of an image

--- src/horn_schunck_method.py
+++ src/horn_schunck_method.py
@@ -1,81 +1,25 @@
-import cv2 as cv
+import cv2
 import numpy as np
-from time import sleep
+import matplotlib.pyplot as plt
+from scipy.ndimage import convolve as filter2
+import os
 
-def compute_image_gradients(img, channelsToUse=[0, 1, 2]):
-    x_kernel = np.array([[-1, 1], [-1, 1]]) * 0.25
-    y_kernel = np.array([[-1, -1], [1, 1]]) * 0.25
-    grad_x = []
-    grad_y = []
+def get_magnitude(u, v):
+    scale = 3
+    sum = 0.0
+    counter = 0.0
 
-    if len(img.shape) == 2:
-        grad_x.append(cv.filter2D(img, cv.CV_64F, x_kernel))
-        grad_y.append(cv.filter2D(img, cv.CV_64F, y_kernel))
-    else:
-        for channel in channelsToUse:
-            grad_x.append(cv.filter2D(img[:, :, channel], -1, x_kernel))
-            grad_y.append(cv.filter2D(img[:, :, channel], -1, y_kernel))
-    # print("passou aqui")
-    return grad_x, grad_y
+    for i in range(0, u.shape[0], 8):
+        for j in range(0, u.shape[1],8):
+            counter += 1
+            dy = v[i,j] * scale
+            dx = u[i,j] * scale
+            magnitude = (dx**2 + dy**2)**0.5
+            sum += magnitude
 
-def compute_temporal_gradients(img1, img2, channelsToUse=[0, 1, 2]):
-    t_kernel = np.ones((2, 2)) * 0.25
-    grad_t = []
-    if len(img1.shape) == 2:
-        grad_t.append(cv.subtract(cv.filter2D(img2, -1, t_kernel), cv.filter2D(img1, -1, t_kernel)))
-    else:
-        for channel in channelsToUse:
-            grad_t.append(cv.subtract(cv.filter2D(img2[:, :, channel], -1, t_kernel), cv.filter2D(img1[:, :, channel], -1, t_kernel)))
-    for i in range(len(grad_t)):
-        grad_t[i] = cv.threshold(grad_t[i], 2, 150, cv.THRESH_BINARY)[1]
-    # print("passou aqui")
-    return grad_t
+    mag_avg = sum / counter
 
-# def check_values_convergence(u, v, u_prev, v_prev, threshold):
-#     return np.mean(np.abs(u - u_prev)) < threshold and np.mean(np.abs(v - v_prev)) < threshold
-
-def compute_optical_flow(img1, img2, alpha, n_iter=50, channelsToUse=[0, 1, 2]):
-    grad_x, grad_y = compute_image_gradients(img1, channelsToUse)
-    grad_t = compute_temporal_gradients(img1, img2, channelsToUse)
-    # for i in range(len(grad_x)):
-    #     cv.imshow("grad_x", grad_x[i])
-    #     cv.imshow("grad_y", grad_y[i])
-    #     cv.imshow("grad_t", grad_t[i])
-    #     cv.waitKey(0)
-
-    u = np.zeros((img1.shape[0], img1.shape[1]))
-    v = np.zeros((img1.shape[0], img1.shape[1]))
-
-    avg_kernel = np.array([[1 / 12, 1 / 6, 1 / 12],
-                            [1 / 6, 0, 1 / 6],
-                            [1 / 12, 1 / 6, 1 / 12]], float)
-
-    for _ in range(n_iter):
-        u_avg = cv.filter2D(u, -1, avg_kernel)
-        v_avg = cv.filter2D(v, -1, avg_kernel)
-        # print(u_avg.shape, v_avg.shape, u.shape, v.shape, grad_x[0].shape, grad_y[0].shape, grad_t[0].shape)
-
-        if len(img1.shape) == 2:
-            u = u_avg - grad_x[0] * ((grad_x[0] * u_avg + grad_y[0] * v_avg + grad_t[0]) / (4*alpha**2 + grad_x[0]**2 + grad_y[0]**2))
-            v = v_avg - grad_y[0] * ((grad_x[0] * u_avg + grad_y[0] * v_avg + grad_t[0]) / (4*alpha**2 + grad_x[0]**2 + grad_y[0]**2))
-        else:
-            u = []
-            v = []
-            for i in range(len(channelsToUse)):
-                u.append(u_avg - grad_x[i] * ((grad_x[i] * u_avg + grad_y[i] * v_avg + grad_t[i]) / (4*alpha**2 + grad_x[i]**2 + grad_y[i]**2)))
-                v.append(v_avg - grad_y[i] * ((grad_x[i] * u_avg + grad_y[i] * v_avg + grad_t[i]) / (4*alpha**2 + grad_x[i]**2 + grad_y[i]**2)))
-            u_sum = np.zeros((img1.shape[0], img1.shape[1]))
-            v_sum = np.zeros((img1.shape[0], img1.shape[1]))
-            for i in range(len(channelsToUse)):
-                u_sum += u[i]
-                v_sum += v[i]
-            u = u_sum / len(channelsToUse)
-            v = v_sum / len(channelsToUse)
-
-        # if check_values_convergence(u, v, u_avg, v_avg, 50):
-        #     break
-    # print("passou aqui")
-    return u, v
+    return mag_avg
 
 def draw_optical_flow(img, u, v, step=16):
     u = u.astype(np.int16)
@@ -83,59 +27,141 @@ def draw_optical_flow(img, u, v, step=16):
     h, w = img.shape[:2]
     for y in range(0, h, step):
         for x in range(0, w, step):
-            cv.arrowedLine(img, (x, y), (x + u[y, x], y + v[y, x]), (0, 255, 0))
+            if np.linalg.norm([u[y, x], v[y, x]]) > 5:
+                cv2.arrowedLine(img, (x, y), (x + u[y, x], y + v[y, x]), (0, 0, 255))
     return img
 
-def video_optical_flow(video_path):
-    cap = cv.VideoCapture(video_path)
-    ret, frame1 = cap.read()
-    frame1 = cv.GaussianBlur(frame1, (5, 5), 0)
-    frame1 = cv.resize(frame1, (0,0), fx=0.1, fy=0.1)
-    prvs = cv.cvtColor(frame1, cv.COLOR_BGR2GRAY)
-    # prvs = frame1
-    while(1):
-        ret, frame2 = cap.read()
-        if not ret:
-            print('No frames grabbed!')
+def draw_quiver(u,v,beforeImg):
+    scale = 3
+    ax = plt.figure().gca()
+    ax.imshow(beforeImg)
+
+    magnitudeAvg = get_magnitude(u, v)
+
+    for i in range(0, u.shape[0], 8):
+        for j in range(0, u.shape[1],8):
+            dy = v[i,j] * scale
+            dx = u[i,j] * scale
+            magnitude = (dx**2 + dy**2)**0.5
+            if magnitude > magnitudeAvg:
+                ax.arrow(j,i, dx, dy, color = 'red')
+
+    plt.draw()
+    plt.show()
+
+def compute_derivatives(img1, img2, channelsToUse=[0, 1, 2]):
+    x_kernel = np.array([[-1, 1], [-1, 1]]) * 0.25
+    y_kernel = np.array([[-1, -1], [1, 1]]) * 0.25
+    t_kernel = np.ones((2, 2)) * 0.25
+
+    if len(img1.shape) == 2:
+        fx = filter2(img1,x_kernel) + filter2(img2,x_kernel)
+        fy = filter2(img1, y_kernel) + filter2(img2, y_kernel)
+        ft = filter2(img1, -t_kernel) + filter2(img2, t_kernel)
+        return [fx,fy, ft]
+    else:
+        fx = np.zeros(img1.shape)
+        fy = np.zeros(img1.shape)
+        ft = np.zeros(img1.shape)
+        for channel in channelsToUse:
+            fx[:,:,channel] = filter2(img1[:,:,channel],x_kernel) + filter2(img2[:,:,channel],x_kernel)
+            fy[:,:,channel] = filter2(img1[:,:,channel], y_kernel) + filter2(img2[:,:,channel], y_kernel)
+            ft[:,:,channel] = filter2(img1[:,:,channel], -t_kernel) + filter2(img2[:,:,channel], t_kernel)
+        return [fx,fy, ft]
+
+def compute_horn_schunck(img1, img2, alpha, delta=10**-1, n_iter=300, channelsToUse=[0, 1, 2]):
+
+    img1 = img1.astype(float)
+    img2 = img2.astype(float)
+
+    img1  = cv2.GaussianBlur(img1, (5, 5), 0)
+    img2 = cv2.GaussianBlur(img2, (5, 5), 0)
+
+    u = np.zeros((img1.shape[0], img1.shape[1]))
+    v = np.zeros((img1.shape[0], img1.shape[1]))
+    fx, fy, ft = compute_derivatives(img1, img2, channelsToUse)
+    avg_kernel = np.array([[1 / 12, 1 / 6, 1 / 12],
+                            [1 / 6, 0, 1 / 6],
+                            [1 / 12, 1 / 6, 1 / 12]], float)
+    for iter_counter in range(n_iter):
+        print("iteration number: ", iter_counter)
+        u_avg = filter2(u, avg_kernel)
+        v_avg = filter2(v, avg_kernel)
+
+        if len(img1.shape) == 2:
+            p = fx * u_avg + fy * v_avg + ft
+            d = 4 * alpha**2 + fx**2 + fy**2
+            prev = u
+
+            u = u_avg - fx * (p / d)
+            v = v_avg - fy * (p / d)
+        else:
+            u_arr = [0, 0, 0]
+            v_arr = [0, 0, 0]
+            for i in channelsToUse:
+                p = fx[:,:,i] * u_avg + fy[:,:,i] * v_avg + ft[:,:,i]
+                d = 4 * alpha**2 + fx[:,:,i]**2 + fy[:,:,i]**2
+                prev = u
+
+                u_arr[i] = (u_avg - fx[:,:,i] * (p / d))
+                v_arr[i] = (v_avg - fy[:,:,i] * (p / d))
+            u_sum = np.zeros((img1.shape[0], img1.shape[1]))
+            v_sum = np.zeros((img1.shape[0], img1.shape[1]))
+            for i in channelsToUse:
+                u_sum += u_arr[i]
+                v_sum += v_arr[i]
+            u = u_sum / len(channelsToUse)
+            v = v_sum / len(channelsToUse)
+
+        diff = np.linalg.norm(u - prev, 2)
+        if  diff < delta or iter_counter > n_iter:
             break
-        frame2 = cv.GaussianBlur(frame2, (5, 5), 0)
-        frame2 = cv.resize(frame2, (0,0), fx=0.1, fy=0.1)
-        next = cv.cvtColor(frame2, cv.COLOR_BGR2GRAY)
-        # next = frame2
-        u, v = compute_optical_flow(prvs, next, 15, 50, [0, 1, 2])
-        img = draw_optical_flow(frame2, u, v, step=8)
-        cv.imshow('frame2', img)
-        k = cv.waitKey(30) & 0xff
-        if k == 27:
+
+    return u, v
+
+def create_entry_frames(video_name):
+    cap = cv2.VideoCapture(video_name)
+    if (cap.isOpened() == False):
+        print("Error opening video stream or file")
+    i = 0  
+    for filename in os.listdir("entry_frames"):
+        os.remove("entry_frames/" + filename)
+    while (cap.isOpened()):
+        ret, frame = cap.read()
+        if ret == True:
+            cv2.imwrite("entry_frames/frame%d.jpg" % i, frame)
+            i += 1
+        else:
             break
-        prvs = next
-    cv.destroyAllWindows()
+    cap.release()
+    cv2.destroyAllWindows()
 
 if __name__ == "__main__":
-    video_optical_flow("assets/videos/video4.mp4")
-    # Read images
-    # img1 = cv.imread("./261.jpg", 0)
-    # img2 = cv.imread("./262.jpg", 0)
-    # if img1 is None or img2 is None:
-    #     raise Exception("Could not read the images.")
+    create_entry_frames("assets/videos/videox5.mp4")
+
+    img2 = cv2.imread("entry_frames/frame184.jpg")
+    img1 = cv2.imread("entry_frames/frame185.jpg")
+    if img1 is None or img2 is None:
+        raise Exception("Could not read the images.")
     
-    # img1 = cv.resize(img1, (0,0), fx=0.5, fy=0.5)
-    # img2 = cv.resize(img2, (0,0), fx=0.5, fy=0.5)
-    # img1_copy = img1.copy()
-    # img2_copy = img2.copy()
-    # img1 = cv.GaussianBlur(img1, (5, 5), 0)
-    # img2 = cv.GaussianBlur(img2, (5, 5), 0)
-    # cv.imshow("Original image", img1_copy)
-    # # cv.imshow("Original image", img2_copy)
-    
-    # # Compute optical flow
-    # u, v = compute_optical_flow(img1, img2, 15, 100)
+    img1 = cv2.resize(img1, (0,0), fx=1, fy=1)
+    img2 = cv2.resize(img2, (0,0), fx=1, fy=1)
+    # img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+    # img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+    # img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2HSV)
+    # img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2HSV)
+    # cv2.imshow("img1", img1)
+    # cv2.imshow("img2", img2)
+    # cv2.waitKey(0)
+    img1_copy = img1.copy()
+    img2_copy = img2.copy()
 
-    # # Draw optical flow
-    # img1_copy = cv.cvtColor(img1_copy, cv.COLOR_GRAY2BGR)
-    # img = draw_optical_flow(img1_copy, u, v, step=8)
 
-    # # Show result
-    # cv.imshow("Optical flow", img)
+    u,v = compute_horn_schunck(img1, img2, alpha = 15, delta = 10**-1, n_iter = 900, channelsToUse = [0, 1, 2])
 
-    # cv.waitKey(0)
+    # img = cv2.cvtColor(img1_copy, cv2.COLOR_GRAY2BGR)
+    img = img1_copy
+
+    draw_quiver(u, v, img)
+
+    cv2.waitKey(0)
